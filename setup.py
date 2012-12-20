9d844e36ae0de1bca5da78f481aac0b67447654a
jashandeep-sohi <joshsohi@gmail.com>
2012-12-19 22:11:24 -0800

    -Made some minor changes to the script. -Added a distutils setup.py script to aid with installation. -Added gpl-3 license

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyphctool',
+      version='0.1',
+      description='A Python tool for finding and setting PHC VIDs.',
+      author='Jashandeep Sohi',
+      author_email='jashandeep.s.sohi@gmail.com',
+      url='https://github.com/jashandeep-sohi/pyphctool',
+      py_modules = ['pyphctoolcore'],
+      scripts = ['pyphctool'],
+      data_files = [('/etc/phc', ['vids.conf'])]
+     )
+
