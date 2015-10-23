d93d5654764db1ad7791eb1db93b927c4cfda915
jashandeep-sohi <joshsohi@gmail.com>
2015-10-23 11:35:32 -0700

    Add vim lines & reformat spacing

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
+# vim: filetype=python3 tabstop=2 expandtab
 
 from distutils.core import setup
 
-setup(name='pyphctool',
-      version='0.1',
-      description='A Python tool for finding and setting PHC VIDs.',
-      author='Jashandeep Sohi',
-      author_email='jashandeep.s.sohi@gmail.com',
-      url='https://github.com/jashandeep-sohi/pyphctool',
-      license='gplv3',
-      py_modules = ['pyphctoolcore'],
-      scripts = ['pyphctool'],
-      data_files = [('/etc/phc', ['vids.conf']),
-                    ('/usr/lib/systemd/system', ['pyphctool-setvids.service'])]
-     )
-
+if __name__ == "__main__":
+  setup(
+    name = 'pyphctool',
+    version = '0.1',
+    description = 'Find, set PHC VIDs for under/over volting CPUs.',
+    author = 'Jashandeep Sohi',
+    author_email = 'jashandeep.s.sohi@gmail.com',
+    url = 'https://github.com/jashandeep-sohi/pyphctool',
+    license = 'GPLv3',
+    py_modules = ['pyphctoolcore'],
+    scripts = ['pyphctool'],
+    data_files = [
+      ('/etc/phc', ['vids.conf']),
+      ('/usr/lib/systemd/system', ['pyphctool-setvids.service'])
+    ]
+  )
